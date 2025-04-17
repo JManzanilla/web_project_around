e6cfefc104694804eafa917a9b0b9cd44667908c
JMG <ing.jesus.manzanilla.gomez@gmail.com>
2025-04-17 17:21:47 -0600

    actualizacion de espacios

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const openProfilePupopBtn = document.querySelector(".profile__lapiz");
+const openProfilePupopBtn = document.querySelector(".profile__lapiz"); //creacion de las variables
 const profileFormPupop = document.querySelector(".pupop");
 const closeProfilePupopBtn = document.querySelector(
   ".pupop__container-btn-close"
@@ -9,15 +9,15 @@ const inputProfileAbout = document.querySelector("#popup__form-about");
 const nameProfileHeader = document.querySelector(".profile__title");
 const aboutProfileHeader = document.querySelector(".profile__title-content");
 const formElement = document.querySelector(".pupop__form");
-
+//creacion de la funcion
 function handleProfileFormSubmit(evt) {
-  evt.preventDefault();
-  nameProfileHeader.textContent = inputProfileName.value;
+  evt.preventDefault(); //llamando a la funcion
+  nameProfileHeader.textContent = inputProfileName.value; // aqui se cambia el nombre del perfil del titulo al ingresar datos al input
   aboutProfileHeader.textContent = inputProfileAbout.value;
-  profileFormPupop.classList.remove("pupop__show");
+  profileFormPupop.classList.remove("pupop__show"); //aqui se retira la clase que se muestra el formulario
 }
-
 openProfilePupopBtn.addEventListener("click", () => {
+  //aqui se crea una funcion que el boton haga click se muestra el formulario
   inputProfileName.value = nameProfileHeader.textContent;
   inputProfileAbout.value = aboutProfileHeader.textContent;
   profileFormPupop.classList.add("pupop__show");
@@ -27,5 +27,4 @@ openProfilePupopBtn.addEventListener("click", () => {
 closeProfilePupopBtn.addEventListener("click", () => {
   profileFormPupop.classList.remove("pupop__show");
 });
-
 formElement.addEventListener("submit", handleProfileFormSubmit);
